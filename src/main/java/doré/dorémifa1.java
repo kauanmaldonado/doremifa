7c8b622505e036d2d2eb3fcb9d62db55ed92ba75
kauanmaldonado <kauan.maldonado16@gmail.com>
2021-08-19 18:07:36 -0300

    Initial commit

---
+++ src/main/java/doré/dorémifa1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+package doré;
+
+public class dorémifa1 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner
+
+        int do1 = 0;
+        int re = 1;
+        int mi = 2;
+        int fa = 3;
+        int sol = 4;
+        int la = 5;
+        int si =6;
+        
+
+
+    }
+
+
+
+
+
+
+
+}
